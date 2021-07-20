92d2d4c7f962f32e6abee008fb9a722acbea5df4
tulasinadupuri2020 <tulasinadupuri2020@gmail.com>
2021-07-20 18:08:59 +0530

    squares from 1 to 10

---
+++ day-1/src/com/practiceground/basics/Squares.java
@@ -0,0 +1,17 @@
+package com.practiceground.basics;
+
+/**
+ * 
+ * @author practice ground
+ * print first 10 squares
+ *
+ */
+public class Squares {
+
+	public static void main(String args[]) {
+		
+		for (int i = 1 ; i <= 10 ; i++) {
+			System.out.printf("%d", i*i).println();
+			}
+	}
+}
